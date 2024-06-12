3c12877f11cc51d26de485444201f010dc75a74b
unknown <velazquezortizcesar@gmail.com>
2024-06-11 23:24:18 -0600

    update

---
+++ scripts/cursorv2.js
@@ -0,0 +1,21 @@
+let estadoCursor = localStorage.getItem("cursor");
+
+if (estadoCursor) {
+    document.body.style.cursor = estadoCursor;
+}
+
+// Función para cambiar el tamaño del cursor
+let btncursor = document.getElementById("cursorBig");
+let cursorActivo = !!estadoCursor;
+
+btncursor.addEventListener('click', function (e) {
+    if (!cursorActivo) {
+        document.body.style.cursor = "url('../images/cursor/cursoracces.png'), auto";
+        cursorActivo = true;
+        localStorage.setItem("cursor", document.body.style.cursor);
+    } else {
+        localStorage.removeItem("cursor");
+        document.body.style.cursor = "auto";
+        cursorActivo = false;
+    }
+});
